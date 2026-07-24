25642821f39d3afaa96b7369e88112f0a6f80f5c
anshsingh06 <anshsingh01227@gmail.com>
2026-07-24 22:23:55 +0530

    Initial Codeforces solutions

---
+++ ATeam.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ATeam{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count =0;
+        for(int i=0;i<n;i++){
+           int a = sc.nextInt();
+           int b = sc.nextInt();
+           int c = sc.nextInt();
+
+           if( a+b+c>1) count++;
+        }
+        System.out.println(count);
+    }
+}
