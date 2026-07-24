fa9858825e9a2c24a02f4da57ab20e1ecf77f3e6
anshsingh06 <anshsingh01227@gmail.com>
2026-07-25 00:24:22 +0530

    new que

---
+++ ANextRound.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ANextRound{
+    public static void main(String[] args ){
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int k = sc.nextInt();
+     int arr[] = new int [n];
+     int count =0;
+     for(int i=0;i<n;i++){
+     arr[i] = sc.nextInt();
+     }
+     int target = arr[k-1];
+     for(int i=0;i<n;i++){
+        if(arr[i]>= target && arr[i]>0) count++;
+     }
+     System.out.println(count);
+    }
+}
